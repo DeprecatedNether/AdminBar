02267cf4c8c2a9aad9c079277230c1021503a90b
DeprecatedNether <deprecatednether@gmail.com>
2014-04-28 16:11:48 +0200

    Add the update class

---
+++ src/pw/deprecatednether/adminbar/UpdateScoreboard.java
@@ -0,0 +1,36 @@
+/*
+ * AdminBar is a Bukkit plugin that displays server information to administrators at all times.
+ * Copyright (C) 2014 DeprecatedNether
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pw.deprecatednether.adminbar;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+
+public class UpdateScoreboard extends BukkitRunnable {
+    @Override
+    public void run() {
+        Objective adminbar = AdminBar.adminbar;
+        Score tps = adminbar.getScore(Bukkit.getServer().getOfflinePlayer("Server TPS"));
+        Score players = adminbar.getScore(Bukkit.getServer().getOfflinePlayer("Online Players"));
+        tps.setScore((int)Math.round(AdminBar.getTPS()));
+        players.setScore(AdminBar.getOnline());
+    }
+}
